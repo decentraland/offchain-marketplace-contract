5eeff5528ee33696b5ecc8e2875d03d5beb78205
Nando <24811313+fzavalia@users.noreply.github.com>
2024-04-07 00:36:30 -0300

    feat: Trade erc721s

--- src/Marketplace.sol
+++ src/Marketplace.sol
@@ -3,19 +3,26 @@ pragma solidity ^0.8.20;
 
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/interfaces/IERC721.sol";
 
 error InvalidSigner();
 
 contract Marketplace is EIP712 {
-    // keccak256("Asset(address contractAddress,uint256 amount)")
-    bytes32 internal constant ASSET_TYPE_HASH = 0x3701fad26cac0416b3131e281bc886d3be98b66236cc4499a46950ad69037484;
-    // keccak256("Trade(Asset[] sent, Asset[] received)Asset(address contractAddress,uint256 amount)")
-    bytes32 internal constant TRADE_TYPE_HASH = 0xfda8eceb8abd3b6d6244a7652f0fb9aa18cd5cc92b769dc5ff72415b859f1b8f;
+    // keccak256("Asset(uint8 assetType,address contractAddress,uint256 amountOrTokenId)")
+    bytes32 internal constant ASSET_TYPE_HASH = 0xca6dc34521a1a16a3c61f9d8d9dbb453951798636529b1dbc7cf94741d77dee3;
+    // keccak256("Trade(Asset[] sent, Asset[] received)Asset(uint8 assetType,address contractAddress,uint256 amountOrTokenId)")
+    bytes32 internal constant TRADE_TYPE_HASH = 0x781907fd28d2e46fc07161b51e5b581feb8befb91ac787388809696a4b68c772;
+
+    enum AssetType {
+        ERC20,
+        ERC721
+    }
 
     struct Asset {
+        AssetType assetType;
         address contractAddress;
-        uint256 amount;
+        uint256 amountOrTokenId;
     }
 
     struct Trade {
@@ -47,7 +54,7 @@ contract Marketplace is EIP712 {
         bytes32[] memory hashes = new bytes32[](_assets.length);
 
         for (uint256 i = 0; i < hashes.length; i++) {
-            hashes[i] = keccak256(abi.encode(ASSET_TYPE_HASH, _assets[i].contractAddress, _assets[i].amount));
+            hashes[i] = keccak256(abi.encode(ASSET_TYPE_HASH, _assets[i].contractAddress, _assets[i].amountOrTokenId));
         }
 
         return abi.encodePacked(hashes);
@@ -55,7 +62,11 @@ contract Marketplace is EIP712 {
 
     function _transferAssets(Asset[] memory _assets, address _from, address _to) internal {
         for (uint256 i = 0; i < _assets.length; i++) {
-            IERC20(_assets[i].contractAddress).transferFrom(_from, _to, _assets[i].amount);
+            if (_assets[i].assetType == AssetType.ERC20) {
+                IERC20(_assets[i].contractAddress).transferFrom(_from, _to, _assets[i].amountOrTokenId);
+            } else {
+                IERC721(_assets[i].contractAddress).safeTransferFrom(_from, _to, _assets[i].amountOrTokenId);
+            }
         }
     }
 }
--- test/Marketplace.t.sol
+++ test/Marketplace.t.sol
@@ -5,6 +5,7 @@ import {Test, console} from "forge-std/Test.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {Marketplace, InvalidSigner} from "../src/Marketplace.sol";
 import {MockERC20} from "../src/mocks/MockERC20.sol";
+import {MockERC721} from "../src/mocks/MockERC721.sol";
 
 contract MarketplaceHarness is Marketplace {
     function getDomainSeparator() external view returns (bytes32) {
@@ -27,7 +28,7 @@ contract MarketplaceHarness is Marketplace {
 contract Accept is Test {
     function setUp() public {}
 
-    function test_Success() public {
+    function test_SuccessERC20() public {
         uint256 signerPk = 0xB0C4;
 
         address signer = vm.addr(signerPk);
@@ -50,10 +51,18 @@ contract Accept is Test {
         assertEq(erc20.balanceOf(caller), 1 ether);
 
         Marketplace.Asset[] memory sent = new Marketplace.Asset[](1);
-        sent[0] = Marketplace.Asset({contractAddress: address(erc20), amount: 0.75 ether});
+        sent[0] = Marketplace.Asset({
+            assetType: Marketplace.AssetType.ERC20,
+            contractAddress: address(erc20),
+            amountOrTokenId: 0.75 ether
+        });
 
         Marketplace.Asset[] memory received = new Marketplace.Asset[](1);
-        received[0] = Marketplace.Asset({contractAddress: address(erc20), amount: 0.25 ether});
+        received[0] = Marketplace.Asset({
+            assetType: Marketplace.AssetType.ERC20,
+            contractAddress: address(erc20),
+            amountOrTokenId: 0.25 ether
+        });
 
         bytes32 digest = MessageHashUtils.toTypedDataHash(
             mkt.getDomainSeparator(),
@@ -72,5 +81,60 @@ contract Accept is Test {
 
         assertEq(erc20.balanceOf(signer), 0.5 ether);
         assertEq(erc20.balanceOf(caller), 1.5 ether);
+    }
+
+    function test_SuccessERC721() public {
+        uint256 signerPk = 0xB0C4;
+
+        address signer = vm.addr(signerPk);
+        address caller = vm.addr(signerPk + 1);
+
+        MarketplaceHarness mkt = new MarketplaceHarness();
+
+        MockERC721 erc721 = new MockERC721();
+
+        erc721.mint(signer, 1);
+        erc721.mint(caller, 2);
+
+        vm.prank(signer);
+        erc721.approve(address(mkt), 1);
+
+        vm.prank(caller);
+        erc721.approve(address(mkt), 2);
+
+        assertEq(erc721.ownerOf(1), signer);
+        assertEq(erc721.ownerOf(2), caller);
+
+        Marketplace.Asset[] memory sent = new Marketplace.Asset[](1);
+        sent[0] = Marketplace.Asset({
+            assetType: Marketplace.AssetType.ERC721,
+            contractAddress: address(erc721),
+            amountOrTokenId: 1
+        });
+
+        Marketplace.Asset[] memory received = new Marketplace.Asset[](1);
+        received[0] = Marketplace.Asset({
+            assetType: Marketplace.AssetType.ERC721,
+            contractAddress: address(erc721),
+            amountOrTokenId: 2
+        });
+
+        bytes32 digest = MessageHashUtils.toTypedDataHash(
+            mkt.getDomainSeparator(),
+            keccak256(abi.encode(mkt.getTradeTypeHash(), mkt.hashAssets(sent), mkt.hashAssets(received)))
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPk, digest);
+
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        Marketplace.Trade memory trade =
+            Marketplace.Trade({signer: signer, signature: signature, sent: sent, received: received});
+
+        vm.prank(caller);
+        mkt.accept(trade);
+
+        assertEq(erc721.ownerOf(1), caller);
+        assertEq(erc721.ownerOf(2), signer);
     }
 }
