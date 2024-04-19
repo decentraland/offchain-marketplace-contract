3ad763550405d63eb6aac61a099b3069c2f33fb9
Nando <24811313+fzavalia@users.noreply.github.com>
2024-04-18 22:51:34 -0300

    fix: Test trade MANA for Estate

--- src/interfaces/IComposableERC721.sol
+++ src/interfaces/IComposableERC721.sol
@@ -5,4 +5,6 @@ import {IERC721} from "lib/openzeppelin-contracts/contracts/interfaces/IERC721.s
 
 interface IComposableERC721 is IERC721 {
     function verifyFingerprint(uint256 _estateId, bytes memory _fingerprint) external returns (bool);
+
+    function getFingerprint(uint256 _estateId) external view returns (bytes32);
 }
