5f1fc4ed61f2c0a48dfe1180776fef80cdf36ae8
Nando <24811313+fzavalia@users.noreply.github.com>
2024-04-19 23:52:25 -0300

    feat: Use SafeERC20 function

--- src/transfers/ERC20Transfer.sol
+++ src/transfers/ERC20Transfer.sol
@@ -1,17 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Marketplace} from "../Marketplace.sol";
-
-error ERC20TransferFailed();
 
 abstract contract ERC20Transfer {
     function _transferERC20(Marketplace.Asset memory _asset, address _from) internal {
-        bool result = IERC20(_asset.contractAddress).transferFrom(_from, _asset.beneficiary, _asset.value);
-
-        if (!result) {
-            revert ERC20TransferFailed();
-        }
+        SafeERC20.safeTransferFrom(IERC20(_asset.contractAddress), _from, _asset.beneficiary, _asset.value);
     }
 }
