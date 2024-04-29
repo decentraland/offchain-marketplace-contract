9dc1fa687195f29de2c051e52a383cdb765dc485
Nando <24811313+fzavalia@users.noreply.github.com>
2024-04-29 17:02:36 -0300

    fix: Add Polygon Tests

---
+++ test/PolygonMarketplace.t.sol
@@ -0,0 +1,425 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {VmSafe} from "forge-std/Vm.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+import {Marketplace} from "../src/Marketplace.sol";
+import {PolygonMarketplace} from "../src/PolygonMarketplace.sol";
+import {IComposable} from "../src/interfaces/IComposable.sol";
+import {ICollectionStore} from "../src/interfaces/ICollectionStore.sol";
+import {ICollection} from "../src/interfaces/ICollection.sol";
+
+contract PolygonMarketplaceHarness is PolygonMarketplace {
+    constructor(address _owner, ICollectionStore _collectionStore) PolygonMarketplace(_owner, _collectionStore) {}
+
+    function eip712Name() external view returns (string memory) {
+        return _EIP712Name();
+    }
+
+    function eip712Version() external view returns (string memory) {
+        return _EIP712Version();
+    }
+
+    function eip712TradeHash(Trade memory _trade) external view returns (bytes32) {
+        return _hashTypedDataV4(_hashTrade(_trade));
+    }
+}
+
+abstract contract PolygonMarketplaceTests is Test {
+    VmSafe.Wallet signer;
+    address other;
+    PolygonMarketplaceHarness marketplace;
+
+    function setUp() public virtual {
+        uint256 forkId = vm.createFork("https://rpc.decentraland.org/polygon", 56395304); // Apr-29-2024 07:23:50 PM +UTC
+        vm.selectFork(forkId);
+
+        signer = vm.createWallet("signer");
+        other = 0x79c63172C7B01A8a5B074EF54428a452E0794E7A;
+        marketplace = new PolygonMarketplaceHarness(
+            0x0E659A116e161d8e502F9036bAbDA51334F2667E, // DAO Agent;
+            ICollectionStore(0x214ffC0f0103735728dc66b61A22e4F163e275ae)
+        );
+    }
+
+    function signTrade(Marketplace.Trade memory _trade) internal view returns (bytes memory) {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(signer.privateKey, marketplace.eip712TradeHash(_trade));
+        return abi.encodePacked(r, s, v);
+    }
+
+    function _getBaseTrades() internal view virtual returns (Marketplace.Trade[] memory) {
+        Marketplace.Trade[] memory trades = new Marketplace.Trade[](1);
+        trades[0].expiration = block.timestamp;
+        trades[0].signer = signer.addr;
+        return trades;
+    }
+}
+
+contract UnsupportedAssetTypeTests is PolygonMarketplaceTests {
+    error UnsupportedAssetType(uint256 _assetType);
+
+    function test_RevertsIfAssetTypeIsInvalid() public {
+        uint256 invalidAssetType = 100;
+
+        Marketplace.Trade[] memory trades = _getBaseTrades();
+        trades[0].sent = new Marketplace.Asset[](1);
+        trades[0].sent[0].assetType = invalidAssetType;
+        trades[0].signature = signTrade(trades[0]);
+
+        vm.prank(other);
+        vm.expectRevert(abi.encodeWithSelector(UnsupportedAssetType.selector, invalidAssetType));
+        marketplace.accept(trades);
+    }
+}
+
+contract TransferERC20Tests is PolygonMarketplaceTests {
+    IERC20 erc20;
+    uint256 erc20Sent;
+    address erc20OriginalHolder;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    error FailedInnerCall();
+
+    function setUp() public override {
+        super.setUp();
+        erc20 = IERC20(0xA1c57f48F0Deb89f569dFbE6E2B7f46D33606fD4);
+        erc20Sent = 1 ether;
+        erc20OriginalHolder = 0x673e6B75a58354919FF5db539AA426727B385D17;
+    }
+
+    function _getBaseTradesForSent() private view returns (Marketplace.Trade[] memory) {
+        Marketplace.Trade[] memory trades = _getBaseTrades();
+        trades[0].sent = new Marketplace.Asset[](1);
+        trades[0].sent[0].assetType = marketplace.ERC20_ID();
+        trades[0].sent[0].contractAddress = address(erc20);
+        trades[0].sent[0].value = erc20Sent;
+        trades[0].signature = signTrade(trades[0]);
+        return trades;
+    }
+
+    function _getBaseTradesForReceived() private view returns (Marketplace.Trade[] memory) {
+        Marketplace.Trade[] memory trades = _getBaseTrades();
+        trades[0].received = new Marketplace.Asset[](1);
+        trades[0].received[0].assetType = marketplace.ERC20_ID();
+        trades[0].received[0].contractAddress = address(erc20);
+        trades[0].received[0].value = erc20Sent;
+        trades[0].signature = signTrade(trades[0]);
+        return trades;
+    }
+
+    function test_RevertsIfSignerHasNotApprovedTheMarketplaceToSendERC20() public {
+        vm.prank(erc20OriginalHolder);
+        erc20.transfer(signer.addr, erc20Sent);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForSent();
+
+        vm.prank(other);
+        vm.expectRevert("ERC20: transfer amount exceeds allowance");
+        marketplace.accept(trades);
+    }
+
+    function test_RevertsIfCallerHasNotApprovedTheMarketplaceToSendERC20() public {
+        vm.prank(erc20OriginalHolder);
+        erc20.transfer(other, erc20Sent);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForReceived();
+
+        vm.prank(other);
+        vm.expectRevert("ERC20: transfer amount exceeds allowance");
+        marketplace.accept(trades);
+    }
+
+    function test_RevertsIfSignerDoesNotHaveEnoughERC20Balance() public {
+        vm.prank(signer.addr);
+        erc20.approve(address(marketplace), erc20Sent);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForSent();
+
+        vm.prank(other);
+        vm.expectRevert("ERC20: transfer amount exceeds balance");
+        marketplace.accept(trades);
+    }
+
+    function test_RevertsIfCallerDoesNotHaveEnoughERC20Balance() public {
+        vm.prank(other);
+        erc20.approve(address(marketplace), erc20Sent);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForReceived();
+
+        vm.prank(other);
+        vm.expectRevert("ERC20: transfer amount exceeds balance");
+        marketplace.accept(trades);
+    }
+
+    function test_TransfersERC20FromSignerToCaller() public {
+        vm.prank(signer.addr);
+        erc20.approve(address(marketplace), erc20Sent);
+
+        vm.prank(erc20OriginalHolder);
+        erc20.transfer(signer.addr, erc20Sent);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForSent();
+
+        vm.prank(other);
+        vm.expectEmit(address(erc20));
+        emit Transfer(signer.addr, other, erc20Sent);
+        marketplace.accept(trades);
+
+        assertEq(erc20.balanceOf(signer.addr), 0);
+        assertEq(erc20.balanceOf(other), erc20Sent);
+    }
+
+    function test_TransfersERC20FromCallerToSigner() public {
+        vm.prank(other);
+        erc20.approve(address(marketplace), erc20Sent);
+
+        vm.prank(erc20OriginalHolder);
+        erc20.transfer(other, erc20Sent);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForReceived();
+
+        vm.prank(other);
+        vm.expectEmit(address(erc20));
+        emit Transfer(other, signer.addr, erc20Sent);
+        marketplace.accept(trades);
+
+        assertEq(erc20.balanceOf(other), 0);
+        assertEq(erc20.balanceOf(signer.addr), erc20Sent);
+    }
+}
+
+contract TransferERC721Tests is PolygonMarketplaceTests {
+    IERC721 erc721;
+    uint256 erc721TokenId;
+    address erc721OriginalHolder;
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    function setUp() public override {
+        super.setUp();
+        erc721 = IERC721(0xDed1e53D7A43aC1844b66c0Ca0F02627EB42e16d);
+        erc721TokenId = 1053122916685571866979180276836704323188950954005491112543109775497;
+        erc721OriginalHolder = 0xc1325a7Cb84b41626eDCC97f5a124B592976cd5d;
+    }
+
+    function _getBaseTradesForSent() private view returns (Marketplace.Trade[] memory) {
+        Marketplace.Trade[] memory trades = _getBaseTrades();
+        trades[0].sent = new Marketplace.Asset[](1);
+        trades[0].sent[0].assetType = marketplace.ERC721_ID();
+        trades[0].sent[0].contractAddress = address(erc721);
+        trades[0].sent[0].value = erc721TokenId;
+        trades[0].signature = signTrade(trades[0]);
+        return trades;
+    }
+
+    function _getBaseTradesForReceived() private view returns (Marketplace.Trade[] memory) {
+        Marketplace.Trade[] memory trades = _getBaseTrades();
+        trades[0].received = new Marketplace.Asset[](1);
+        trades[0].received[0].assetType = marketplace.ERC721_ID();
+        trades[0].received[0].contractAddress = address(erc721);
+        trades[0].received[0].value = erc721TokenId;
+        trades[0].signature = signTrade(trades[0]);
+        return trades;
+    }
+
+    function test_RevertsIfSignerHasNotApprovedTheMarketplaceToSendERC721() public {
+        vm.prank(erc721OriginalHolder);
+        erc721.transferFrom(erc721OriginalHolder, signer.addr, erc721TokenId);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForSent();
+
+        vm.prank(other);
+        vm.expectRevert();
+        marketplace.accept(trades);
+    }
+
+    function test_RevertsIfCallerHasNotApprovedTheMarketplaceToSendERC721() public {
+        vm.prank(erc721OriginalHolder);
+        erc721.transferFrom(erc721OriginalHolder, other, erc721TokenId);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForReceived();
+
+        vm.prank(other);
+        vm.expectRevert();
+        marketplace.accept(trades);
+    }
+
+    function test_RevertsIfSignerDoesNotHaveTheERC721Token() public {
+        vm.prank(signer.addr);
+        erc721.setApprovalForAll(address(marketplace), true);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForSent();
+
+        vm.prank(other);
+        vm.expectRevert();
+        marketplace.accept(trades);
+    }
+
+    function test_RevertsIfCallerDoesNotHaveTheERC721Token() public {
+        vm.prank(other);
+        erc721.setApprovalForAll(address(marketplace), true);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForReceived();
+
+        vm.prank(other);
+        vm.expectRevert();
+        marketplace.accept(trades);
+    }
+
+    function test_TransfersERC721FromSignerToCaller() public {
+        vm.prank(signer.addr);
+        erc721.setApprovalForAll(address(marketplace), true);
+
+        vm.prank(erc721OriginalHolder);
+        erc721.transferFrom(erc721OriginalHolder, signer.addr, erc721TokenId);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForSent();
+
+        vm.prank(other);
+        vm.expectEmit(address(erc721));
+        emit Transfer(signer.addr, other, erc721TokenId);
+        marketplace.accept(trades);
+
+        assertEq(erc721.ownerOf(erc721TokenId), other);
+    }
+
+    function test_TransfersERC721FromCallerToSigner() public {
+        vm.prank(other);
+        erc721.setApprovalForAll(address(marketplace), true);
+
+        vm.prank(erc721OriginalHolder);
+        erc721.transferFrom(erc721OriginalHolder, other, erc721TokenId);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForReceived();
+
+        vm.prank(other);
+        vm.expectEmit(address(erc721));
+        emit Transfer(other, signer.addr, erc721TokenId);
+        marketplace.accept(trades);
+
+        assertEq(erc721.ownerOf(erc721TokenId), signer.addr);
+    }
+}
+
+contract TransferCollectionItemTests is PolygonMarketplaceTests {
+    ICollection collection;
+    uint256 collectionItemId;
+    address collectionItemOriginalCreator;
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    error NotCreator();
+
+    function setUp() public override {
+        super.setUp();
+        collection = ICollection(0xDed1e53D7A43aC1844b66c0Ca0F02627EB42e16d);
+        collectionItemId = 10;
+        collectionItemOriginalCreator = 0x3cf368FaeCdb4a4E542c0efD17850ae133688C2a;
+    }
+
+    function _getBaseTradesForSent() private view returns (Marketplace.Trade[] memory) {
+        Marketplace.Trade[] memory trades = _getBaseTrades();
+        trades[0].sent = new Marketplace.Asset[](1);
+        trades[0].sent[0].assetType = marketplace.COLLECTION_ITEM_ID();
+        trades[0].sent[0].contractAddress = address(collection);
+        trades[0].sent[0].value = collectionItemId;
+        trades[0].signature = signTrade(trades[0]);
+        return trades;
+    }
+
+    function _getBaseTradesForReceived() private view returns (Marketplace.Trade[] memory) {
+        Marketplace.Trade[] memory trades = _getBaseTrades();
+        trades[0].received = new Marketplace.Asset[](1);
+        trades[0].received[0].assetType = marketplace.COLLECTION_ITEM_ID();
+        trades[0].received[0].contractAddress = address(collection);
+        trades[0].received[0].value = collectionItemId;
+        trades[0].signature = signTrade(trades[0]);
+        return trades;
+    }
+
+    function test_RevertsIfSignerIsNotTheCreator() public {
+        Marketplace.Trade[] memory trades = _getBaseTradesForSent();
+
+        vm.prank(other);
+        vm.expectRevert(NotCreator.selector);
+        marketplace.accept(trades);
+    }
+
+    function test_RevertsIfCallerIsNotTheCreator() public {
+        Marketplace.Trade[] memory trades = _getBaseTradesForReceived();
+
+        vm.prank(other);
+        vm.expectRevert(NotCreator.selector);
+        marketplace.accept(trades);
+    }
+
+    function test_RevertsIfTheMarketplaceIsNotCollectionMinterOfTheSentAsset() public {
+        vm.prank(collectionItemOriginalCreator);
+        collection.transferCreatorship(signer.addr);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForSent();
+
+        vm.prank(other);
+        vm.expectRevert("_issueToken: CALLER_CAN_NOT_MINT");
+        marketplace.accept(trades);
+    }
+
+    function test_RevertsIfTheMarketplaceIsNotCollectionMinterOfTheReceivedAsset() public {
+        vm.prank(collectionItemOriginalCreator);
+        collection.transferCreatorship(other);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForReceived();
+
+        vm.prank(other);
+        vm.expectRevert("_issueToken: CALLER_CAN_NOT_MINT");
+        marketplace.accept(trades);
+    }
+
+    function test_MintsAndTranfersTheItemToTheCaller() public {
+        vm.prank(collectionItemOriginalCreator);
+        collection.transferCreatorship(signer.addr);
+
+        vm.prank(signer.addr);
+        address[] memory setMintersMinters = new address[](1);
+        setMintersMinters[0] = address(marketplace);
+        bool[] memory setMintersValues = new bool[](1);
+        setMintersValues[0] = true;
+        collection.setMinters(setMintersMinters, setMintersValues);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForSent();
+
+        vm.prank(other);
+        vm.expectEmit(address(collection));
+        uint256 expectedTokenId = 1053122916685571866979180276836704323188950954005491112543109775772;
+        emit Transfer(address(0), other, expectedTokenId);
+        marketplace.accept(trades);
+
+        assertEq(collection.ownerOf(expectedTokenId), other);
+    }
+
+    function test_MintsAndTranfersTheItemToTheSigner() public {
+        vm.prank(collectionItemOriginalCreator);
+        collection.transferCreatorship(other);
+
+        vm.prank(other);
+        address[] memory setMintersMinters = new address[](1);
+        setMintersMinters[0] = address(marketplace);
+        bool[] memory setMintersValues = new bool[](1);
+        setMintersValues[0] = true;
+        collection.setMinters(setMintersMinters, setMintersValues);
+
+        Marketplace.Trade[] memory trades = _getBaseTradesForReceived();
+
+        vm.prank(other);
+        vm.expectEmit(address(collection));
+        uint256 expectedTokenId = 1053122916685571866979180276836704323188950954005491112543109775772;
+        emit Transfer(address(0), signer.addr, expectedTokenId);
+        marketplace.accept(trades);
+
+        assertEq(collection.ownerOf(expectedTokenId), signer.addr);
+    }
+}
