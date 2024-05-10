3eae50ed668ff6face6ad77277dee8755102aeb6
Nando <24811313+fzavalia@users.noreply.github.com>
2024-05-09 23:31:35 -0300

    fix: Add tests for discount type

--- src/coupons/CollectionDiscountCoupon.sol
+++ src/coupons/CollectionDiscountCoupon.sol
@@ -9,11 +9,15 @@ import {ICollection} from "src/marketplace/ICollection.sol";
 
 /// @notice Coupon that allows creators to apply discounts to Trades involving their Collections.
 contract CollectionDiscountCoupon is CouponTypes, MarketplaceTypes {
+    uint256 public constant DISCOUNT_TYPE_RATE = 1;
+    uint256 public constant DISCOUNT_TYPE_FLAT = 2;
+
     /// @notice Schema Discount.
     /// @param rate The rate of the discount. Must be over 1 million instead of 100. For example, 10% would be 100_000.
     /// @param root The Merkle root of all the Collections that this Coupon will be valid for.
     struct CollectionDiscountCouponData {
-        uint256 rate;
+        uint256 discountType;
+        uint256 discount;
         bytes32 root;
     }
 
@@ -26,6 +30,7 @@ contract CollectionDiscountCoupon is CouponTypes, MarketplaceTypes {
     error TradesWithOneSentCollectionItemAllowed();
     error InvalidProof(address _collectionAddress);
     error SignerIsNotTheCreator(address _signer, address _creator);
+    error InvalidDiscountType(uint256 _discountType);
 
     /// @notice Applies the discount to the received assets of the Trade.
     /// @param _trade The Trade to apply the discount to.
@@ -35,7 +40,11 @@ contract CollectionDiscountCoupon is CouponTypes, MarketplaceTypes {
     /// Only Trades with one sent Collection item are allowed.
     ///
     /// All received assets will have the discount applied. !!!EVEN IF THEY ARE NOT ERC20s!!!.
-    function applyCoupon(MarketplaceTypes.Trade memory _trade, CouponTypes.Coupon memory _coupon) external view returns (MarketplaceTypes.Trade memory) {
+    function applyCoupon(MarketplaceTypes.Trade memory _trade, CouponTypes.Coupon memory _coupon)
+        external
+        view
+        returns (MarketplaceTypes.Trade memory)
+    {
         if (_trade.sent.length != 1) {
             revert TradesWithOneSentCollectionItemAllowed();
         }
@@ -63,7 +72,14 @@ contract CollectionDiscountCoupon is CouponTypes, MarketplaceTypes {
 
         for (uint256 i = 0; i < _trade.received.length; i++) {
             uint256 originalPrice = _trade.received[i].value;
-            _trade.received[i].value = originalPrice - originalPrice * data.rate / 1_000_000;
+
+            if (data.discountType == DISCOUNT_TYPE_RATE) {
+                _trade.received[i].value = originalPrice - originalPrice * data.discount / 1_000_000;
+            } else if (data.discountType == DISCOUNT_TYPE_FLAT) {
+                _trade.received[i].value = originalPrice - data.discount;
+            } else {
+                revert InvalidDiscountType(data.discountType);
+            }
         }
 
         return _trade;
--- test/coupons/CollectionDiscountCoupon.t.sol
+++ test/coupons/CollectionDiscountCoupon.t.sol
@@ -161,8 +161,9 @@ contract ApplyCollectionDiscountCouponTests is CollectionDiscountCouponTests {
 
     function test_AppliesTheDiscountToAllReceivedAssetValues() public {
         CollectionDiscountCouponHarness.CollectionDiscountCouponData memory collectionDiscountCouponData;
+        collectionDiscountCouponData.discountType = collectionDiscountCoupon.DISCOUNT_TYPE_RATE();
+        collectionDiscountCouponData.discount = 500_000;
         collectionDiscountCouponData.root = 0x56980103ca6f02663aeaa6b3895be0e41e507731e5a2655d3da8c9c8618ccc92;
-        collectionDiscountCouponData.rate = 500_000;
 
         CollectionDiscountCouponHarness.CollectionDiscountCouponCallerData memory collectionDiscountCouponCallerData;
         collectionDiscountCouponCallerData.proof = new bytes32[](3);
@@ -192,10 +193,110 @@ contract ApplyCollectionDiscountCouponTests is CollectionDiscountCouponTests {
         assertEq(updatedTrade.received[2].value, 1.5 ether);
     }
 
+    function test_AppliesTheDiscountToAllReceivedAssetValues_RevertsIfDiscountGreaterThan1_000_000() public {
+        CollectionDiscountCouponHarness.CollectionDiscountCouponData memory collectionDiscountCouponData;
+        collectionDiscountCouponData.discountType = collectionDiscountCoupon.DISCOUNT_TYPE_RATE();
+        collectionDiscountCouponData.discount = 1_000_001;
+        collectionDiscountCouponData.root = 0x56980103ca6f02663aeaa6b3895be0e41e507731e5a2655d3da8c9c8618ccc92;
+
+        CollectionDiscountCouponHarness.CollectionDiscountCouponCallerData memory collectionDiscountCouponCallerData;
+        collectionDiscountCouponCallerData.proof = new bytes32[](3);
+        collectionDiscountCouponCallerData.proof[0] = 0xa7c46294ffa3fad92dc8422b2e38b688ccf1b86172f5beaf864af9368d2844e5;
+        collectionDiscountCouponCallerData.proof[1] = 0xb8e277bcec6ddfe5a414b2200b3abcb1d3ee435c66531e8f21898f36a7ed122f;
+        collectionDiscountCouponCallerData.proof[2] = 0x7747f5b3dcece1341b1470c482b95e4b5565365e4169abeb52162734e62147cf;
+
+        CollectionDiscountCouponHarness.Coupon memory coupon;
+        coupon.data = abi.encode(collectionDiscountCouponData);
+        coupon.callerData = abi.encode(collectionDiscountCouponCallerData);
+
+        CollectionDiscountCouponHarness.Trade memory trade;
+        trade.signer = signer;
+        trade.sent = new CollectionDiscountCouponHarness.Asset[](1);
+        trade.sent[0].contractAddress = address(mockCollection);
+        trade.received = new CollectionDiscountCouponHarness.Asset[](3);
+        trade.received[0].value = 1 ether;
+        trade.received[1].value = 2 ether;
+        trade.received[2].value = 3 ether;
+
+        mockCollection.transferCreatorship(signer);
+
+        vm.expectRevert(); // [FAIL. Reason: panic: arithmetic underflow or overflow (0x11)]
+        collectionDiscountCoupon.applyCoupon(trade, coupon);
+    }
+
+    function test_AppliesTheDiscountToAllReceivedAssetValues_Allows0Discount() public {
+        CollectionDiscountCouponHarness.CollectionDiscountCouponData memory collectionDiscountCouponData;
+        collectionDiscountCouponData.discountType = collectionDiscountCoupon.DISCOUNT_TYPE_RATE();
+        collectionDiscountCouponData.discount = 0;
+        collectionDiscountCouponData.root = 0x56980103ca6f02663aeaa6b3895be0e41e507731e5a2655d3da8c9c8618ccc92;
+
+        CollectionDiscountCouponHarness.CollectionDiscountCouponCallerData memory collectionDiscountCouponCallerData;
+        collectionDiscountCouponCallerData.proof = new bytes32[](3);
+        collectionDiscountCouponCallerData.proof[0] = 0xa7c46294ffa3fad92dc8422b2e38b688ccf1b86172f5beaf864af9368d2844e5;
+        collectionDiscountCouponCallerData.proof[1] = 0xb8e277bcec6ddfe5a414b2200b3abcb1d3ee435c66531e8f21898f36a7ed122f;
+        collectionDiscountCouponCallerData.proof[2] = 0x7747f5b3dcece1341b1470c482b95e4b5565365e4169abeb52162734e62147cf;
+
+        CollectionDiscountCouponHarness.Coupon memory coupon;
+        coupon.data = abi.encode(collectionDiscountCouponData);
+        coupon.callerData = abi.encode(collectionDiscountCouponCallerData);
+
+        CollectionDiscountCouponHarness.Trade memory trade;
+        trade.signer = signer;
+        trade.sent = new CollectionDiscountCouponHarness.Asset[](1);
+        trade.sent[0].contractAddress = address(mockCollection);
+        trade.received = new CollectionDiscountCouponHarness.Asset[](3);
+        trade.received[0].value = 1 ether;
+        trade.received[1].value = 2 ether;
+        trade.received[2].value = 3 ether;
+
+        mockCollection.transferCreatorship(signer);
+
+        CollectionDiscountCouponHarness.Trade memory updatedTrade = collectionDiscountCoupon.applyCoupon(trade, coupon);
+
+        assertEq(updatedTrade.received[0].value, 1 ether);
+        assertEq(updatedTrade.received[1].value, 2 ether);
+        assertEq(updatedTrade.received[2].value, 3 ether);
+    }
+
+    function test_AppliesTheDiscountToAllReceivedAssetValues_Allows1_000_000Discount() public {
+        CollectionDiscountCouponHarness.CollectionDiscountCouponData memory collectionDiscountCouponData;
+        collectionDiscountCouponData.discountType = collectionDiscountCoupon.DISCOUNT_TYPE_RATE();
+        collectionDiscountCouponData.discount = 1_000_000;
+        collectionDiscountCouponData.root = 0x56980103ca6f02663aeaa6b3895be0e41e507731e5a2655d3da8c9c8618ccc92;
+
+        CollectionDiscountCouponHarness.CollectionDiscountCouponCallerData memory collectionDiscountCouponCallerData;
+        collectionDiscountCouponCallerData.proof = new bytes32[](3);
+        collectionDiscountCouponCallerData.proof[0] = 0xa7c46294ffa3fad92dc8422b2e38b688ccf1b86172f5beaf864af9368d2844e5;
+        collectionDiscountCouponCallerData.proof[1] = 0xb8e277bcec6ddfe5a414b2200b3abcb1d3ee435c66531e8f21898f36a7ed122f;
+        collectionDiscountCouponCallerData.proof[2] = 0x7747f5b3dcece1341b1470c482b95e4b5565365e4169abeb52162734e62147cf;
+
+        CollectionDiscountCouponHarness.Coupon memory coupon;
+        coupon.data = abi.encode(collectionDiscountCouponData);
+        coupon.callerData = abi.encode(collectionDiscountCouponCallerData);
+
+        CollectionDiscountCouponHarness.Trade memory trade;
+        trade.signer = signer;
+        trade.sent = new CollectionDiscountCouponHarness.Asset[](1);
+        trade.sent[0].contractAddress = address(mockCollection);
+        trade.received = new CollectionDiscountCouponHarness.Asset[](3);
+        trade.received[0].value = 1 ether;
+        trade.received[1].value = 2 ether;
+        trade.received[2].value = 3 ether;
+
+        mockCollection.transferCreatorship(signer);
+
+        CollectionDiscountCouponHarness.Trade memory updatedTrade = collectionDiscountCoupon.applyCoupon(trade, coupon);
+
+        assertEq(updatedTrade.received[0].value, 0);
+        assertEq(updatedTrade.received[1].value, 0);
+        assertEq(updatedTrade.received[2].value, 0);
+    }
+
     function test_AppliesTheDiscountToAllReceivedAssetValues_MerkleTreeWithOneValue() public {
         CollectionDiscountCouponHarness.CollectionDiscountCouponData memory collectionDiscountCouponData;
+        collectionDiscountCouponData.discountType = collectionDiscountCoupon.DISCOUNT_TYPE_RATE();
+        collectionDiscountCouponData.discount = 500_000;
         collectionDiscountCouponData.root = 0x7e321b7ae61d2fe49a2b9c8ba4d76b1b7f74d5eb773b09d8e23120a69998b51c;
-        collectionDiscountCouponData.rate = 500_000;
 
         CollectionDiscountCouponHarness.CollectionDiscountCouponCallerData memory collectionDiscountCouponCallerData;
 
@@ -219,5 +320,70 @@ contract ApplyCollectionDiscountCouponTests is CollectionDiscountCouponTests {
         assertEq(updatedTrade.received[0].value, 0.5 ether);
         assertEq(updatedTrade.received[1].value, 1 ether);
         assertEq(updatedTrade.received[2].value, 1.5 ether);
+    }
+
+    function test_AppliesTheDiscountToAllReceivedAssetValues_FlatDiscount() public {
+        CollectionDiscountCouponHarness.CollectionDiscountCouponData memory collectionDiscountCouponData;
+        collectionDiscountCouponData.discountType = collectionDiscountCoupon.DISCOUNT_TYPE_FLAT();
+        collectionDiscountCouponData.discount = 0.5 ether;
+        collectionDiscountCouponData.root = 0x56980103ca6f02663aeaa6b3895be0e41e507731e5a2655d3da8c9c8618ccc92;
+
+        CollectionDiscountCouponHarness.CollectionDiscountCouponCallerData memory collectionDiscountCouponCallerData;
+        collectionDiscountCouponCallerData.proof = new bytes32[](3);
+        collectionDiscountCouponCallerData.proof[0] = 0xa7c46294ffa3fad92dc8422b2e38b688ccf1b86172f5beaf864af9368d2844e5;
+        collectionDiscountCouponCallerData.proof[1] = 0xb8e277bcec6ddfe5a414b2200b3abcb1d3ee435c66531e8f21898f36a7ed122f;
+        collectionDiscountCouponCallerData.proof[2] = 0x7747f5b3dcece1341b1470c482b95e4b5565365e4169abeb52162734e62147cf;
+
+        CollectionDiscountCouponHarness.Coupon memory coupon;
+        coupon.data = abi.encode(collectionDiscountCouponData);
+        coupon.callerData = abi.encode(collectionDiscountCouponCallerData);
+
+        CollectionDiscountCouponHarness.Trade memory trade;
+        trade.signer = signer;
+        trade.sent = new CollectionDiscountCouponHarness.Asset[](1);
+        trade.sent[0].contractAddress = address(mockCollection);
+        trade.received = new CollectionDiscountCouponHarness.Asset[](3);
+        trade.received[0].value = 1 ether;
+        trade.received[1].value = 2 ether;
+        trade.received[2].value = 3 ether;
+
+        mockCollection.transferCreatorship(signer);
+
+        CollectionDiscountCouponHarness.Trade memory updatedTrade = collectionDiscountCoupon.applyCoupon(trade, coupon);
+
+        assertEq(updatedTrade.received[0].value, 0.5 ether);
+        assertEq(updatedTrade.received[1].value, 1.5 ether);
+        assertEq(updatedTrade.received[2].value, 2.5 ether);
+    }
+
+    function test_AppliesTheDiscountToAllReceivedAssetValues_FlatDiscount_RevertsIfDiscountIsGreaterThanOriginalPrice() public {
+        CollectionDiscountCouponHarness.CollectionDiscountCouponData memory collectionDiscountCouponData;
+        collectionDiscountCouponData.discountType = collectionDiscountCoupon.DISCOUNT_TYPE_FLAT();
+        collectionDiscountCouponData.discount = 1000 ether;
+        collectionDiscountCouponData.root = 0x56980103ca6f02663aeaa6b3895be0e41e507731e5a2655d3da8c9c8618ccc92;
+
+        CollectionDiscountCouponHarness.CollectionDiscountCouponCallerData memory collectionDiscountCouponCallerData;
+        collectionDiscountCouponCallerData.proof = new bytes32[](3);
+        collectionDiscountCouponCallerData.proof[0] = 0xa7c46294ffa3fad92dc8422b2e38b688ccf1b86172f5beaf864af9368d2844e5;
+        collectionDiscountCouponCallerData.proof[1] = 0xb8e277bcec6ddfe5a414b2200b3abcb1d3ee435c66531e8f21898f36a7ed122f;
+        collectionDiscountCouponCallerData.proof[2] = 0x7747f5b3dcece1341b1470c482b95e4b5565365e4169abeb52162734e62147cf;
+
+        CollectionDiscountCouponHarness.Coupon memory coupon;
+        coupon.data = abi.encode(collectionDiscountCouponData);
+        coupon.callerData = abi.encode(collectionDiscountCouponCallerData);
+
+        CollectionDiscountCouponHarness.Trade memory trade;
+        trade.signer = signer;
+        trade.sent = new CollectionDiscountCouponHarness.Asset[](1);
+        trade.sent[0].contractAddress = address(mockCollection);
+        trade.received = new CollectionDiscountCouponHarness.Asset[](3);
+        trade.received[0].value = 1 ether;
+        trade.received[1].value = 2 ether;
+        trade.received[2].value = 3 ether;
+
+        mockCollection.transferCreatorship(signer);
+
+        vm.expectRevert(); // [FAIL. Reason: panic: arithmetic underflow or overflow (0x11)]
+        collectionDiscountCoupon.applyCoupon(trade, coupon);
     }
 }
