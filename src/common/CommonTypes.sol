00df5b8b5bf29f1e9627add893a1f2f90eb21c2a
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2024-07-22 16:11:46 -0300

    fix: Change license to MIT

--- src/common/CommonTypes.sol
+++ src/common/CommonTypes.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
 /// @notice Types used by many contracts in this project.
--- src/marketplace/interfaces/IRoyaltiesManager.sol
+++ src/marketplace/interfaces/IRoyaltiesManager.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
 import {IERC721} from "@openzeppelin/contracts/interfaces/IERC721.sol";
