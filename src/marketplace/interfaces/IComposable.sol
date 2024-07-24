0f71dfb3190c462d881dd2cfcb2d3958660c5a9e
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2024-07-24 01:28:28 -0300

    fix: Make verifyFingerprint in IComposable, view

--- src/marketplace/interfaces/IComposable.sol
+++ src/marketplace/interfaces/IComposable.sol
@@ -5,7 +5,7 @@ import {IERC721} from "@openzeppelin/contracts/interfaces/IERC721.sol";
 
 /// @notice Interface for ERC721 Composable contracts.
 interface IComposable is IERC721 {
-    function verifyFingerprint(uint256 _estateId, bytes memory _fingerprint) external returns (bool);
+    function verifyFingerprint(uint256 _estateId, bytes memory _fingerprint) external view returns (bool);
 
     function getFingerprint(uint256 _estateId) external view returns (bytes32);
 }
