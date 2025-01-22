63363e1f6a0c629832508867fc16adb65e57108e
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-01-22 18:12:54 -0300

    feat: Add pause, unpause and forward
    - pause and unpause can only be called by pauser
    - forwarder by caller
    - checks current mana coupon signer with the signer role

--- src/forwarders/ManaCouponMarketplaceForwarder.sol
+++ src/forwarders/ManaCouponMarketplaceForwarder.sol
@@ -1,6 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract ManaCouponMarketplaceForwarder {
-    
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+
+contract ManaCouponMarketplaceForwarder is AccessControl, Pausable {
+    using ECDSA for bytes32;
+
+    bytes32 public constant CALLER_ROLE = keccak256("CALLER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
+
+    struct ManaCoupon {
+        uint256 amount;
+        uint256 expiration;
+        bytes signature;
+    }
+
+    mapping(bytes32 => uint256) public consumedCoupons;
+
+    error InvalidSigner(address _signer);
+    error CouponExpired(uint256 _currentTime);
+
+    constructor(address _caller, address _pauser, address _signer) {
+        _grantRole(CALLER_ROLE, _caller);
+        _grantRole(PAUSER_ROLE, _pauser);
+        _grantRole(SIGNER_ROLE, _signer);
+    }
+
+    function pause() external onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() external onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function forward(ManaCoupon calldata _coupon) external onlyRole(CALLER_ROLE) whenNotPaused {
+        bytes32 hashedCoupon = keccak256(abi.encode(_coupon.amount, _coupon.expiration));
+        address signer = hashedCoupon.recover(_coupon.signature);
+
+        if (!hasRole(SIGNER_ROLE, signer)) {
+            revert InvalidSigner(signer);
+        }
+
+        if (_coupon.expiration < block.timestamp) {
+            revert CouponExpired(block.timestamp);
+        }
+
+        consumedCoupons[keccak256(_coupon.signature)] += _coupon.amount;
+    }
 }
---
+++ test/forwarders/ManaCouponMarketplaceForwarder.t.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {VmSafe} from "forge-std/Vm.sol";
+
+import {ManaCouponMarketplaceForwarder} from "src/forwarders/ManaCouponMarketplaceForwarder.sol";
+
+contract ManaCouponMarketplaceForwarderHarness is ManaCouponMarketplaceForwarder {
+    constructor(address _caller, address _pauser, address _signer) ManaCouponMarketplaceForwarder(_caller, _pauser, _signer) {}
+}
+
+contract ManaCouponMarketplaceForwarderTests is Test {
+    address other;
+    address caller;
+    address pauser;
+    VmSafe.Wallet signer;
+    VmSafe.Wallet otherSigner;
+
+    ManaCouponMarketplaceForwarderHarness forwarder;
+    ManaCouponMarketplaceForwarderHarness.ManaCoupon coupon;
+
+    error AccessControlUnauthorizedAccount(address account, bytes32 neededRole);
+    error EnforcedPause();
+    error InvalidSigner(address _signer);
+    error CouponExpired(uint256 _currentTime);
+
+    function _sign(uint256 _pk, ManaCouponMarketplaceForwarderHarness.ManaCoupon memory _coupon) private pure returns (bytes memory) {
+        bytes32 hashedCoupon = keccak256(abi.encode(_coupon.amount, _coupon.expiration));
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(_pk, hashedCoupon);
+
+        return abi.encodePacked(r, s, v);
+    }
+
+    function setUp() public {
+        other = makeAddr("other");
+        caller = makeAddr("caller");
+        pauser = makeAddr("pauser");
+        signer = vm.createWallet("signer");
+        otherSigner = vm.createWallet("otherSigner");
+
+        coupon.amount = 100;
+        coupon.expiration = block.timestamp + 1 days;
+        coupon.signature = _sign(signer.privateKey, coupon);
+
+        forwarder = new ManaCouponMarketplaceForwarderHarness(caller, pauser, signer.addr);
+    }
+
+    function test_pause_RevertsIfSenderIsNotPauser() public {
+        vm.expectRevert(abi.encodeWithSelector(AccessControlUnauthorizedAccount.selector, other, forwarder.PAUSER_ROLE()));
+        vm.prank(other);
+        forwarder.pause();
+    }
+
+    function test_unpause_RevertsIfSenderIsNotPauser() public {
+        vm.expectRevert(abi.encodeWithSelector(AccessControlUnauthorizedAccount.selector, other, forwarder.PAUSER_ROLE()));
+        vm.prank(other);
+        forwarder.unpause();
+    }
+
+    function test_forward_RevertsIfSenderIsNotAuthorizedCaller() public {
+        vm.expectRevert(abi.encodeWithSelector(AccessControlUnauthorizedAccount.selector, other, forwarder.CALLER_ROLE()));
+        vm.prank(other);
+        forwarder.forward(coupon);
+    }
+
+    function test_forward_RevertsIfPaused() public {
+        vm.prank(pauser);
+        forwarder.pause();
+
+        vm.expectRevert(EnforcedPause.selector);
+        vm.prank(caller);
+        forwarder.forward(coupon);
+    }
+
+    function test_forward_RevertsIfMessageSignerIsInvalid() public {
+        coupon.signature = _sign(otherSigner.privateKey, coupon);
+
+        vm.expectRevert(abi.encodeWithSelector(InvalidSigner.selector, otherSigner.addr));
+        vm.prank(caller);
+        forwarder.forward(coupon);
+    }
+
+    function test_forward_AddsTheCouponAmountToTheConsumedCouponsMapping() public {
+        vm.prank(caller);
+        forwarder.forward(coupon);
+
+        assertEq(forwarder.consumedCoupons(keccak256(coupon.signature)), coupon.amount);
+    }
+
+    function test_forward_RevertsIfCouponIsExpired() public {
+        coupon.expiration = block.timestamp - 1;
+        coupon.signature = _sign(signer.privateKey, coupon);
+
+        vm.expectRevert(abi.encodeWithSelector(CouponExpired.selector, block.timestamp));
+        vm.prank(caller);
+        forwarder.forward(coupon);
+    }
+}
