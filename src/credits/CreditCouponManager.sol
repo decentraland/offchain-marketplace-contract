42f348138aa00f61db796c19e866db93474c2097
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-01-23 19:11:17 -0300

    feat: Pause, Deny and Accept logic

--- src/credits/CreditCouponManager.sol
+++ src/credits/CreditCouponManager.sol
@@ -2,15 +2,24 @@
 pragma solidity 0.8.20;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 import {MarketplaceWithCouponManager} from "src/marketplace/MarketplaceWithCouponManager.sol";
 import {MarketplaceTypes} from "src/marketplace/MarketplaceTypes.sol";
 import {CouponTypes} from "src/coupons/CouponTypes.sol";
 import {ICollectionFactory} from "src/credits/interfaces/ICollectionFactory.sol";
 
-contract CreditCouponManager is MarketplaceTypes, CouponTypes {
-    uint256 private constant TRADE_TYPE_LISTING = 1;
-    uint256 private constant TRADE_TYPE_BID = 2;
+contract CreditCouponManager is MarketplaceTypes, CouponTypes, ReentrancyGuard, Pausable, AccessControl {
+    using SafeERC20 for IERC20;
+    using ECDSA for bytes32;
+
+    bytes32 public constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant DENIER_ROLE = keccak256("DENIER_ROLE");
 
     struct CreditCoupon {
         uint256 amount;
@@ -25,50 +34,121 @@ contract CreditCouponManager is MarketplaceTypes, CouponTypes {
 
     ICollectionFactory[] public factories;
 
-    mapping(bytes32 => uint256) public manaUsedFromCredits;
+    mapping(bytes32 => uint256) public spentCouponCredits;
 
-    constructor(MarketplaceWithCouponManager _marketplace, IERC20 _mana, ICollectionFactory[] memory _factories) {
+    mapping(address => bool) public denyList;
+
+    constructor(address _owner, address _signer, address _pauser, address _denier, MarketplaceWithCouponManager _marketplace, IERC20 _mana, ICollectionFactory[] memory _factories) {
+        _grantRole(DEFAULT_ADMIN_ROLE, _owner);
+        _grantRole(SIGNER_ROLE, _signer);
+        _grantRole(PAUSER_ROLE, _pauser);
+        _grantRole(PAUSER_ROLE, _owner);
+        _grantRole(DENIER_ROLE, _denier);
+        _grantRole(DENIER_ROLE, _owner);
+
         marketplace = _marketplace;
         mana = _mana;
         factories = _factories;
     }
 
-    function accept(Trade[] calldata _trades, CreditCoupon[][] calldata _credits) external {
-        marketplace.accept(_trades);
+    function pause() external onlyRole(PAUSER_ROLE) {
+        _pause();
     }
 
-    function acceptWithCoupon(Trade[] calldata _trades, Coupon[] calldata _coupons, CreditCoupon[][] calldata _credits) external {
-        marketplace.acceptWithCoupon(_trades, _coupons);
+    function unpause() external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _unpause();
     }
 
-    function _validateAndComputeTradeType(Trade calldata _trade) private view returns (uint256 tradeType) {
-        for (uint256 i = 0; i < _trade.sent.length; i++) {
-            Asset calldata asset = _trade.sent[i];
+    function deny(address[] calldata _user) external onlyRole(DENIER_ROLE) {
+        for (uint256 i = 0; i < _user.length; i++) {
+            denyList[_user[i]] = true;
+        }
+    }
 
-            bool isMana = asset.contractAddress == address(mana);
-            bool isDecentralandItem = _isDecentralandItem(asset.contractAddress);
+    function undeny(address[] calldata _user) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        for (uint256 i = 0; i < _user.length; i++) {
+            denyList[_user[i]] = false;
+        }
+    }
 
-            if (i == 0) {
-                if (isMana) {
-                    tradeType = TRADE_TYPE_BID;
-                } else if (isDecentralandItem) {
-                    tradeType = TRADE_TYPE_LISTING;
+    function accept(Trade[] calldata _trades, CreditCoupon[] calldata _creditCoupons) external nonReentrant whenNotPaused {
+        address sender = _msgSender();
+
+        if (denyList[sender]) {
+            revert("Sender is denied");
+        }
+
+        if (_trades.length == 0 || _creditCoupons.length == 0) {
+            revert("Invalid argument arrays length");
+        }
+
+        for (uint256 i = 0; i < _trades.length; i++) {
+            Trade calldata trade = _trades[i];
+
+            if (trade.sent.length < 1 || trade.received.length != 1) {
+                revert("Invalid assets length");
+            }
+
+            if (trade.received[0].contractAddress != address(mana)) {
+                revert("Invalid received asset");
+            }
+
+            for (uint256 j = 0; j < trade.sent.length; j++) {
+                Asset calldata asset = trade.sent[j];
+
+                if (!_isDecentralandItem(asset.contractAddress) || asset.beneficiary == address(0)) {
+                    revert("Invalid sent asset");
                 }
-            } else if (tradeType == 0 || isMana || tradeType == TRADE_TYPE_BID && isDecentralandItem || !isDecentralandItem) {
-                revert("Invalid asset");
             }
         }
 
-        for (uint256 i = 0; i < _trade.received.length; i++) {
-            Asset calldata asset = _trade.received[i];
+        uint256 oldBalance = mana.balanceOf(address(this));
 
-            bool isMana = asset.contractAddress == address(mana);
-            bool isDecentralandItem = _isDecentralandItem(asset.contractAddress);
+        marketplace.accept(_trades);
 
-            if (tradeType == TRADE_TYPE_BID && !isDecentralandItem || tradeType == TRADE_TYPE_LISTING && !isMana || tradeType == TRADE_TYPE_LISTING && _trade.received.length > 1) {
-                revert("Invalid asset");
-            }
+        uint256 newBalance = mana.balanceOf(address(this));
+
+        uint256 totalManaTransferred = oldBalance - newBalance;
+
+        if (totalManaTransferred == 0) {
+            revert("No mana was transferred");
         }
+
+        uint256 totalCreditSpent = 0;
+
+        for (uint256 i = 0; i < _creditCoupons.length; i++) {
+            CreditCoupon calldata creditCoupon = _creditCoupons[i];
+
+            if (creditCoupon.amount == 0) {
+                revert("Invalid coupon amount");
+            }
+
+            if (block.timestamp > creditCoupon.expiration) {
+                revert("Coupon has expired");
+            }
+
+            bytes32 couponHash = keccak256(abi.encode(sender, creditCoupon.amount, creditCoupon.expiration, creditCoupon.salt));
+
+            if (!hasRole(SIGNER_ROLE, couponHash.recover(creditCoupon.signature))) {
+                revert("Invalid coupon signature");
+            }
+
+            bytes32 couponSigHash = keccak256(creditCoupon.signature);
+
+            uint256 totalManaTransferredAndCreditSpentDiff = totalManaTransferred - totalCreditSpent;
+
+            uint256 spendableCredit = creditCoupon.amount - spentCouponCredits[couponSigHash];
+
+            uint256 creditToBeSpent = totalManaTransferredAndCreditSpentDiff > spendableCredit ? spendableCredit : totalManaTransferredAndCreditSpentDiff;
+
+            totalCreditSpent += creditToBeSpent;
+
+            spentCouponCredits[couponSigHash] += creditToBeSpent;
+        }
+
+        mana.safeTransfer(sender, totalCreditSpent);
+
+        mana.safeTransferFrom(sender, address(this), totalManaTransferred - totalCreditSpent);
     }
 
     function _isDecentralandItem(address _contractAddress) private view returns (bool) {
