931dd1117b6c0867f9ccc2d735a4698be51074dc
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-02-09 15:37:53 -0300

    fix: Inherit new strategy in credit manager

--- src/credits/CreditManager.sol
+++ src/credits/CreditManager.sol
@@ -5,22 +5,25 @@ import {CreditManagerBase} from "src/credits/CreditManagerBase.sol";
 import {CollectionStoreStrategy} from "src/credits/strategies/CollectionStoreStrategy.sol";
 import {MarketplaceStrategy} from "src/credits/strategies/MarketplaceStrategy.sol";
 import {OffchainMarketplaceStrategy} from "src/credits/strategies/OffchainMarketplaceStrategy.sol";
+import {ArbitraryCallStrategy} from "src/credits/strategies/ArbitraryCallStrategy.sol";
 import {ICollectionStore} from "src/credits/interfaces/ICollectionStore.sol";
 import {IMarketplace} from "src/credits/interfaces/IMarketplace.sol";
 import {MarketplaceWithCouponManager} from "src/marketplace/MarketplaceWithCouponManager.sol";
 import {IManaUsdRateProvider} from "src/credits/rates/interfaces/IManaUsdRateProvider.sol";
-
-contract CreditManager is CollectionStoreStrategy, MarketplaceStrategy, OffchainMarketplaceStrategy {
+contract CreditManager is CollectionStoreStrategy, MarketplaceStrategy, OffchainMarketplaceStrategy, ArbitraryCallStrategy {
     constructor(
         ICollectionStore _collectionStore,
         IMarketplace _marketplace,
         MarketplaceWithCouponManager _offchainMarketplace,
         IManaUsdRateProvider _manaUsdRateProvider,
+        address _arbitraryCallSigner,
+        address _arbitraryCallRevoker,
         BaseConstructorParams memory _baseConstructorParams
     )
         CollectionStoreStrategy(ICollectionStore(_collectionStore))
         MarketplaceStrategy(_marketplace)
         OffchainMarketplaceStrategy(_offchainMarketplace, _manaUsdRateProvider)
+        ArbitraryCallStrategy(_arbitraryCallSigner, _arbitraryCallRevoker)
         CreditManagerBase(_baseConstructorParams)
     {}
 }
