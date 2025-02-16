08f6a7a46f1fec920703879864c9a06938182cfc
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-02-16 01:24:39 -0300

    fix: Rename functions

--- src/credits/CreditManagerBase.sol
+++ src/credits/CreditManagerBase.sol
@@ -179,12 +179,12 @@ abstract contract CreditManagerBase is Pausable, AccessControl, NativeMetaTransa
     }
 
     /// @notice Allows the owner to withdraw any ERC20 token from the contract.
-    function withdrawErc20(IERC20 _token, uint256 _amount, address _beneficiary) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function withdrawERC20(IERC20 _token, uint256 _amount, address _beneficiary) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _token.safeTransfer(_beneficiary, _amount);
     }
 
     /// @notice Allows the owner to withdraw any ERC721 token from the contract.
-    function withdrawErc721(IERC721 _token, uint256 _tokenId, address _beneficiary) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    function withdrawERC721(IERC721 _token, uint256 _tokenId, address _beneficiary) external onlyRole(DEFAULT_ADMIN_ROLE) {
         _token.safeTransferFrom(address(this), _beneficiary, _tokenId);
     }
 
