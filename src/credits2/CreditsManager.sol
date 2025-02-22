7f2ad8aa6dbe1ef7f8213bc0e128b2ea94fe44c1
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-02-22 19:09:10 -0300

    fix: Add comments

--- src/credits2/CreditsManager.sol
+++ src/credits2/CreditsManager.sol
@@ -55,6 +55,7 @@ abstract contract CreditsManager is AccessControl, Pausable, ReentrancyGuard {
     error RevokedCredit(bytes32 _creditId);
     error InvalidSignature(bytes32 _creditId, address _recoveredSigner);
     error SpentCredit(bytes32 _creditId);
+    error NoMANATransfer();
 
     /// @param _owner The owner of the contract.
     /// @param _signer The address that can sign credits.
@@ -62,6 +63,7 @@ abstract contract CreditsManager is AccessControl, Pausable, ReentrancyGuard {
     /// @param _denier The address that can deny users from using credits.
     /// @param _revoker The address that can revoke credits.
     /// @param _mana The address of the MANA token.
+
     constructor(address _owner, address _signer, address _pauser, address _denier, address _revoker, IERC20 _mana) {
         _grantRole(DEFAULT_ADMIN_ROLE, _owner);
 
@@ -116,9 +118,16 @@ abstract contract CreditsManager is AccessControl, Pausable, ReentrancyGuard {
         emit CreditRevoked(_credit);
     }
 
+    /// @notice Use credits to pay for external calls that transfer MANA.
+    /// @notice Credits will be spent until the MANA transferred is equal to the credited value.
+    /// @notice Any unused credit value can be used on a future call.
+    /// @dev The signatures must have been signed by the signer role.
+    /// @param _credits The credits to use.
+    /// @param _signatures The signatures of the credits.
     function useCredits(Credit[] calldata _credits, bytes[] calldata _signatures) external nonReentrant {
         address sender = _msgSender();
 
+        // Check if the user is denied from using credits.
         if (isDenied[sender]) {
             revert DeniedUser(sender);
         }
@@ -127,10 +136,17 @@ abstract contract CreditsManager is AccessControl, Pausable, ReentrancyGuard {
 
         uint256 balanceBefore = mana.balanceOf(self);
 
+        // Perform the external call, which is handled by the inheriting contract.
         _externalCall();
 
         uint256 manaTransferred = mana.balanceOf(self) - balanceBefore;
 
+        // Check that mana was transferred out of the contract.
+        if (manaTransferred == 0) {
+            revert NoMANATransfer();
+        }
+
+        // Track how much has been credited to cover the MANA transferred.
         uint256 creditedValue = 0;
 
         for (uint256 i = 0; i < _credits.length; i++) {
@@ -140,35 +156,47 @@ abstract contract CreditsManager is AccessControl, Pausable, ReentrancyGuard {
 
             bytes32 signatureHash = keccak256(signature);
 
+            // Check that the credit has not expired.
             if (block.timestamp > credit.expiresAt) {
                 revert CreditExpired(signatureHash);
             }
 
+            // Check that the credit has not been revoked.
             if (isRevoked[signatureHash]) {
                 revert RevokedCredit(signatureHash);
             }
 
             address recoveredSigner = keccak256(abi.encode(sender, block.chainid, self, credit)).recover(signature);
 
+            // Check that the signature has been signed by the signer role.
             if (!hasRole(SIGNER_ROLE, recoveredSigner)) {
                 revert InvalidSignature(signatureHash, recoveredSigner);
             }
 
             uint256 creditRemainingValue = credit.value - spentValue[signatureHash];
 
+            // Check that the credit has not been completely spent.
             if (creditRemainingValue == 0) {
                 revert SpentCredit(signatureHash);
             }
 
-            uint256 diff = manaTransferred - creditedValue;
+            // Calculate how much MANA is left to be credited.
+            uint256 uncreditedValue = manaTransferred - creditedValue;
 
-            uint256 creditValueToSpend = diff < creditRemainingValue ? diff : creditRemainingValue;
+            // Determine how much of the credit to spend.
+            // If the value of the credit is higher than the required amount, only spend the required amount and leave the rest for future calls.
+            uint256 creditValueToSpend = uncreditedValue < creditRemainingValue ? uncreditedValue : creditRemainingValue;
 
             spentValue[signatureHash] += creditValueToSpend;
 
             creditedValue += creditValueToSpend;
 
             emit CreditUsed(signatureHash, credit, creditValueToSpend);
+
+            // If enough credits have been spent, break out of the loop so it doesn't iterate over the remaining credits.
+            if (creditedValue == manaTransferred) {
+                break;
+            }
         }
 
         if (manaTransferred > creditedValue) {
