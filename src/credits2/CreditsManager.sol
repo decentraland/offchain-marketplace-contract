806eb34528ff14c72dcbc9653c399d9cf3d605fa
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-02-22 18:55:40 -0300

    fix: Transfer mana from sender to contract if not enough credits

--- src/credits2/CreditsManager.sol
+++ src/credits2/CreditsManager.sol
@@ -4,9 +4,13 @@ pragma solidity 0.8.20;
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract CreditsManager is AccessControl, Pausable {
+abstract contract CreditsManager is AccessControl, Pausable, ReentrancyGuard {
     using ECDSA for bytes32;
+    using SafeERC20 for IERC20;
 
     /// @notice The role that can sign credits.
     bytes32 public constant SIGNER_ROLE = keccak256("SIGNER_ROLE");
@@ -27,6 +31,13 @@ contract CreditsManager is AccessControl, Pausable {
     /// @dev The key is the hash of the credit signature.
     mapping(bytes32 => bool) public isRevoked;
 
+    /// @notice The address of the MANA token.
+    IERC20 public immutable mana;
+
+    /// @notice The amount of MANA value used on each credit.
+    /// @dev The key is the hash of the credit signature.
+    mapping(bytes32 => uint256) spentValue;
+
     /// @param _value How much ERC20 the credit is worth.
     /// @param _expiresAt The timestamp when the credit expires.
     struct Credit {
@@ -34,29 +45,24 @@ contract CreditsManager is AccessControl, Pausable {
         uint256 expiresAt;
     }
 
-    /// @notice Emitted when a user is denied from using credits.
-    /// @param _user The address of the user that was denied from using credits.
     event UserDenied(address indexed _user);
-
-    /// @notice Emitted when a user is allowed to use credits.
-    /// @param _user The address of the user that was allowed to use credits.
     event UserAllowed(address indexed _user);
+    event CreditRevoked(bytes32 indexed _creditId);
+    event CreditUsed(bytes32 indexed _creditId, Credit _credit, uint256 _value);
 
-    /// @notice Emitted when a credit is revoked.
-    /// @param _credit The hash of the credit signature that was revoked.
-    event CreditRevoked(bytes32 indexed _credit);
-
-    error CreditExpired();
+    error CreditExpired(bytes32 _creditId);
     error DeniedUser(address _user);
-    error RevokedCredit(bytes32 _credit);
-    error InvalidSignature(bytes32 _credit, address _recoveredSigner);
+    error RevokedCredit(bytes32 _creditId);
+    error InvalidSignature(bytes32 _creditId, address _recoveredSigner);
+    error SpentCredit(bytes32 _creditId);
 
     /// @param _owner The owner of the contract.
     /// @param _signer The address that can sign credits.
     /// @param _pauser The address that can pause the contract.
     /// @param _denier The address that can deny users from using credits.
     /// @param _revoker The address that can revoke credits.
-    constructor(address _owner, address _signer, address _pauser, address _denier, address _revoker) {
+    /// @param _mana The address of the MANA token.
+    constructor(address _owner, address _signer, address _pauser, address _denier, address _revoker, IERC20 _mana) {
         _grantRole(DEFAULT_ADMIN_ROLE, _owner);
 
         _grantRole(SIGNER_ROLE, _signer);
@@ -69,6 +75,8 @@ contract CreditsManager is AccessControl, Pausable {
 
         _grantRole(REVOKER_ROLE, _revoker);
         _grantRole(REVOKER_ROLE, _owner);
+
+        mana = _mana;
     }
 
     /// @notice Pauses the contract.
@@ -108,33 +116,65 @@ contract CreditsManager is AccessControl, Pausable {
         emit CreditRevoked(_credit);
     }
 
-    function useCredits(Credit[] calldata _credits, bytes[] calldata _signatures) external {
+    function useCredits(Credit[] calldata _credits, bytes[] calldata _signatures) external nonReentrant {
         address sender = _msgSender();
+
+        if (isDenied[sender]) {
+            revert DeniedUser(sender);
+        }
+
+        address self = address(this);
+
+        uint256 balanceBefore = mana.balanceOf(self);
+
+        _externalCall();
+
+        uint256 manaTransferred = mana.balanceOf(self) - balanceBefore;
+
+        uint256 creditedValue = 0;
 
         for (uint256 i = 0; i < _credits.length; i++) {
             Credit calldata credit = _credits[i];
-
-            if (block.timestamp > credit.expiresAt) {
-                revert CreditExpired();
-            }
-
-            if (isDenied[sender]) {
-                revert DeniedUser(sender);
-            }
 
             bytes calldata signature = _signatures[i];
 
             bytes32 signatureHash = keccak256(signature);
 
+            if (block.timestamp > credit.expiresAt) {
+                revert CreditExpired(signatureHash);
+            }
+
             if (isRevoked[signatureHash]) {
                 revert RevokedCredit(signatureHash);
             }
 
-            address recoveredSigner = keccak256(abi.encode(sender, block.chainid, credit)).recover(signature);
+            address recoveredSigner = keccak256(abi.encode(sender, block.chainid, self, credit)).recover(signature);
 
             if (!hasRole(SIGNER_ROLE, recoveredSigner)) {
                 revert InvalidSignature(signatureHash, recoveredSigner);
             }
+
+            uint256 creditRemainingValue = credit.value - spentValue[signatureHash];
+
+            if (creditRemainingValue == 0) {
+                revert SpentCredit(signatureHash);
+            }
+
+            uint256 diff = manaTransferred - creditedValue;
+
+            uint256 creditValueToSpend = diff < creditRemainingValue ? diff : creditRemainingValue;
+
+            spentValue[signatureHash] += creditValueToSpend;
+
+            creditedValue += creditValueToSpend;
+
+            emit CreditUsed(signatureHash, credit, creditValueToSpend);
+        }
+
+        if (manaTransferred > creditedValue) {
+            mana.safeTransferFrom(sender, self, manaTransferred - creditedValue);
         }
     }
+
+    function _externalCall() internal virtual;
 }
