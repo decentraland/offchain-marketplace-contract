a563ef7e0af1d3cb2c5817f334c7ed8e16e6c908
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-02-25 23:15:01 -0300

    fix: Replace old credits

--- src/credits/interfaces/ICollectionStore.sol
+++ src/credits/interfaces/ICollectionStore.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {ICollection} from "src/marketplace/interfaces/ICollection.sol";
+struct ItemToBuy {
+    address collection;
+    uint256[] ids;
+    uint256[] prices;
+    address[] beneficiaries;
+}
 
 interface ICollectionStore {
-    struct ItemToBuy {
-        ICollection collection;
-        uint256[] ids;
-        uint256[] prices;
-        address[] beneficiaries;
-    }
-
-    function buy(ItemToBuy[] calldata _itemsToBuy) external;
+    function buy(ItemToBuy[] memory _itemsToBuy) external;
 }
--- src/credits2/interfaces/ICollectionFactory.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
-
-interface ICollectionFactory {
-    function isCollectionFromFactory(address _collection) external view returns (bool);
-}
