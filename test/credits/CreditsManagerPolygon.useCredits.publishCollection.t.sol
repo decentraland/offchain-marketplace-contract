527f6c3c06636c9bcc073fb14342bf25d1a74dc0
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-06-02 11:53:55 -0300

    test: Test publishing a collection with a meta tx

--- test/credits/CreditsManagerPolygon.useCredits.publishCollection.t.sol
+++ test/credits/CreditsManagerPolygon.useCredits.publishCollection.t.sol
@@ -58,8 +58,8 @@ contract CreditsManagerPolygonUseCreditsPublishCollectionTest is CreditsManagerP
         // Create Collection Items
         ICollectionManager.ItemParam[] memory items = new ICollectionManager.ItemParam[](1);
         items[0].rarity = "common";
-        items[0].price = 1 ether;
-        items[0].beneficiary = address(this);
+        items[0].price = 0;
+        items[0].beneficiary = address(0);
         items[0].metadata = "metadata";
 
         // Create Collection External Call
@@ -121,9 +121,88 @@ contract CreditsManagerPolygonUseCreditsPublishCollectionTest is CreditsManagerP
         assertEq(callerBalancerAfter, callerBalanceBefore);
         assertEq(creditsManagerBalanceAfter, creditsManagerBalanceBefore - expectedPublishCollectionCost);
 
-        address expectedPublishedCollectionAddress = 0xdfaf732cD3623A0Bd4E120E6c87BF930875d220b;
+        address expectedPublishedCollectionAddress = 0x0B3f057FcCC5b9ef368A6Acac654B0C4cB95F06b;
 
         assertEq(ICollection(expectedPublishedCollectionAddress).creator(), address(this));
+        assertEq(ICollection(expectedPublishedCollectionAddress).owner(), forwarder);
+    }
+
+    function test_useCredits_PublishCollection_MetaTx() public {
+        bytes4 createCollectionSelector = ICollectionManager(collectionManager).createCollection.selector;
+
+        // Create Collection Items
+        ICollectionManager.ItemParam[] memory items = new ICollectionManager.ItemParam[](1);
+        items[0].rarity = "common";
+        items[0].price = 0;
+        items[0].beneficiary = address(0);
+        items[0].metadata = "metadata";
+
+        // Create Collection External Call
+        CreditsManagerPolygon.ExternalCall memory externalCall;
+        externalCall.target = collectionManager;
+        externalCall.selector = createCollectionSelector;
+        externalCall.expiresAt = type(uint256).max;
+        externalCall.data = abi.encode(
+            forwarder,
+            collectionFactoryV3,
+            bytes32(0),
+            "CreditsManagerPublishCollectionCollectionName",
+            "CreditsManagerPublishCollectionCollectionSymbol",
+            "CreditsManagerPublishCollectionBaseURI",
+            metaTxSigner,
+            items
+        );
+
+        // External Call Signature
+        bytes memory customExternalCallSignature =
+            _sign(customExternalCallSignerPk, keccak256(abi.encode(metaTxSigner, block.chainid, address(creditsManager), externalCall)));
+
+        // Credits
+        CreditsManagerPolygon.Credit[] memory credits = new CreditsManagerPolygon.Credit[](1);
+        credits[0].value = 1000 ether;
+        credits[0].expiresAt = type(uint256).max;
+
+        // Credits Signatures
+        bytes[] memory creditsSignatures = new bytes[](1);
+        creditsSignatures[0] = _sign(creditsSignerPk, keccak256(abi.encode(metaTxSigner, block.chainid, address(creditsManager), credits[0])));
+
+        // Use Credits Args
+        CreditsManagerPolygon.UseCreditsArgs memory args;
+        args.credits = credits;
+        args.creditsSignatures = creditsSignatures;
+        args.externalCall = externalCall;
+        args.customExternalCallSignature = customExternalCallSignature;
+        args.maxCreditedValue = type(uint256).max;
+
+        vm.prank(manaHolder);
+        IERC20(mana).transfer(address(creditsManager), 1000 ether);
+
+        vm.prank(owner);
+        creditsManager.allowCustomExternalCall(collectionManager, createCollectionSelector, true);
+
+        vm.prank(owner);
+        creditsManager.updateMaxManaCreditedPerHour(type(uint256).max);
+
+        uint256 callerBalanceBefore = IERC20(mana).balanceOf(metaTxSigner);
+        uint256 creditsManagerBalanceBefore = IERC20(mana).balanceOf(address(creditsManager));
+
+        bytes memory metaTxFunctionData = abi.encodeCall(creditsManager.useCredits, (args));
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(metaTxSignerPk, creditsManager.metaTxToTypedHashData(metaTxSigner, metaTxFunctionData));
+        bytes memory metaTxSignature = abi.encodePacked(r, s, v);
+
+        creditsManager.executeMetaTransaction(metaTxSigner, metaTxFunctionData, metaTxSignature);
+
+        uint256 callerBalancerAfter = IERC20(mana).balanceOf(metaTxSigner);
+        uint256 creditsManagerBalanceAfter = IERC20(mana).balanceOf(address(creditsManager));
+
+        uint256 expectedPublishCollectionCost = 363151721157582426361;
+
+        assertEq(callerBalancerAfter, callerBalanceBefore);
+        assertEq(creditsManagerBalanceAfter, creditsManagerBalanceBefore - expectedPublishCollectionCost);
+
+        address expectedPublishedCollectionAddress = 0x23b6e935D32C6E36dE8438fE341902715A61e989;
+
+        assertEq(ICollection(expectedPublishedCollectionAddress).creator(), metaTxSigner);
         assertEq(ICollection(expectedPublishedCollectionAddress).owner(), forwarder);
     }
 }
